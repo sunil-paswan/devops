42217b294f5ac22b8dde6b4a6396a57e7201a966
sunil <linuspaswan@gmail.com>
2021-06-20 19:13:41 +0530

    updated

--- src/test/java/com/example/devops/web/WelcomeControllerTest.java
+++ src/test/java/com/example/devops/web/WelcomeControllerTest.java
@@ -1,11 +1,9 @@
 package com.example.devops.web;
-
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.junit.Assert.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,6 +14,7 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
